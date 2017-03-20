d1c5cad688de76e83aea3a956edb261b24e99860
Marco Peereboom <marco@peereboom.us>
2017-03-19 21:56:54 -0500

    Add a dummy device for testing.

---
+++ device/dummy/dummy.go
@@ -0,0 +1,32 @@
+package dummy
+
+import (
+	"errors"
+
+	"github.com/marcopeereboom/toyz80/device"
+)
+
+var (
+	ErrInvalidAddress = errors.New("invalid address")
+)
+
+// Dummy is a dummy device for testing.
+type Dummy struct {
+	last byte
+}
+
+var (
+	_ device.Device = (*Dummy)(nil)
+)
+
+func (d *Dummy) Write(address, data byte) {
+	d.last = data
+}
+
+func (d *Dummy) Read(address byte) byte {
+	return d.last
+}
+
+func New() (interface{}, error) {
+	return &Dummy{last: 0xff}, nil
+}
